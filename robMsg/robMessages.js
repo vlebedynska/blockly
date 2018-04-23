80031bd8d5058895bbe74e1925baeca2f5417c08
Patrick Werner <boon.werner@gmail.com>
2018-04-23 15:30:51 +0200

    OpenRoberta/robertalab#859 added missing info to tooltip

--- robMsg/robMessages.js
+++ robMsg/robMessages.js
@@ -1129,7 +1129,7 @@ Blockly.Msg.SLOT_RIGHT_YAW = 'right yaw';
 Blockly.Msg.SLOT_RIGHT_YAW_PITCH = 'right yaw pitch';
 
 Blockly.Msg.FOURDIGITDISPLAY = '4-Digit Display';
-Blockly.Msg.FOURDIGITDISPLAY_SHOW_TOOLTIP = 'Displays a number [0-9999] on the Grove 4-Digit Display by Seeed. Position [0-3] represents the starting position of the number.';
+Blockly.Msg.FOURDIGITDISPLAY_SHOW_TOOLTIP = 'Displays a number [0-9999] on the Grove 4-Digit Display by Seeed. Position [0-3] represents the starting position of the number. The display has to be connected to the A1 (right) Grove connector.';
 Blockly.Msg.FOURDIGITDISPLAY_CLEAR_TOOLTIP = 'Clears the Grove 4-Digit Display by Seeed.'
 Blockly.Msg.COLON = 'colon';
 Blockly.Msg.FROM_POSITION = 'from position';
