ba8d84c473ee20a964729ddd130bf5cf52080f43
Patrick Werner <boon.werner@gmail.com>
2018-05-11 11:01:15 +0200

    OpenRoberta/robertalab#879 added calliope pins

--- blocks/mbedActions.js
+++ blocks/mbedActions.js
@@ -494,17 +494,20 @@ Blockly.Blocks['mbedActions_write_to_pin'] = {
             return;
         }
         if (protocol === 'ANALOG') {
-            var pins = [ [ '1', '1' ], [ '2', '2' ], [ 'A1', '5' ] ];
+            var pins = [ [ 'P1', '1' ], [ 'P2', '2' ], [ 'A1', '5' ],
+                         [ 'C4', 'C4'], [ 'C5', 'C5'], [ 'C6', 'C6'] ];
             var pinField = this.getField("PIN");
             pinField.menuGenerator_ = pins;
             pinField.setValue("1");
-            pinField.setText('1');
+            pinField.setText('P1');
         } else if (protocol === 'DIGITAL') {
-            var pins = [ [ '0', '0' ], [ '1', '1' ], [ '2', '2' ], [ '3', '3' ], [ 'A0', '4' ], [ 'A1', '5' ] ];
+            var pins = [ [ 'P0', '0' ], [ 'P1', '1' ], [ 'P2', '2' ], [ 'P3', '3' ], [ 'A0', '4' ], [ 'A1', '5' ],
+                         [ 'C04', 'C04'], [ 'C05', 'C05'], [ 'C06', 'C06'], [ 'C07', 'C07'], [ 'C08', 'C08'], [ 'C09', 'C09'],
+                         [ 'C10', 'C10'], [ 'C11', 'C11'], [ 'C12', 'C12'] ];
             var pinField = this.getField("PIN");
             pinField.menuGenerator_ = pins;
             pinField.setValue("0");
-            pinField.setText('0');
+            pinField.setText('P0');
         }
     }
 };
@@ -526,7 +529,9 @@ Blockly.Blocks['mbedActions_pin_set_pull'] = {
     init : function() {
         this.setColour(Blockly.CAT_ACTION_RGB);
         var pull = new Blockly.FieldDropdown([ [ Blockly.Msg.PIN_PULL_UP, 'UP' ], [ Blockly.Msg.PIN_PULL_DOWN, 'DOWN' ], [ Blockly.Msg.PIN_PULL_NONE, 'NONE' ] ]);
-        var pins = new Blockly.FieldDropdown([ [ '0', '0' ], [ '1', '1' ], [ '2', '2' ], [ '3', '3' ], [ 'A0', '4' ], [ 'A1', '5' ] ]);
+        var pins = new Blockly.FieldDropdown([ [ 'P0', '0' ], [ 'P1', '1' ], [ 'P2', '2' ], [ 'P3', '3' ], [ 'A0', '4' ], [ 'A1', '5' ],
+                                               [ 'C04', 'C04'], [ 'C05', 'C05'], [ 'C06', 'C06'], [ 'C07', 'C07'], [ 'C08', 'C08'], [ 'C09', 'C09'],
+                                               [ 'C10', 'C10'], [ 'C11', 'C11'], [ 'C12', 'C12'] ]);
         this.appendDummyInput().appendField(Blockly.Msg.SET + ' ' + Blockly.Msg.PIN_PULL).appendField(pull, 'PIN_PULL')
         .appendField(Blockly.Msg.ON + ' ' + Blockly.Msg.SENSOR_PIN).appendField(pins, 'PIN_PORT'); // shouldnt be called PIN, would need a special clause in xml.js like mbedActions_write_to_pin
         this.setPreviousStatement(true);
