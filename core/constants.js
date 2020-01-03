eb7a5261f40f3472e86242804f79e8f134789949
Artem Vinokurov <artem.vinokurov@iais.fraunhofer.de>
2020-01-03 11:45:53 +0100

    issue OpenRoberta/openroberta-lab#381 - removes string datatype for bob3

--- core/constants.js
+++ core/constants.js
@@ -305,7 +305,7 @@ Blockly.TYPE_DROPDOWN = function(device, opt_handler) {
         });
     case 'bob3':
         return new Blockly.FieldDropdown([ [ Blockly.Msg.VARIABLES_TYPE_NUMBER, 'Number' ], [ Blockly.Msg.VARIABLES_TYPE_BOOLEAN, 'Boolean' ],
-                [ Blockly.Msg.VARIABLES_TYPE_STRING, 'String' ], [ Blockly.Msg.VARIABLES_TYPE_COLOUR, 'Colour' ] ], function(option) {
+                [ Blockly.Msg.VARIABLES_TYPE_COLOUR, 'Colour' ] ], function(option) {
             if (option && this.sourceBlock_.getFieldValue('TYPE') !== option) {
                 this.sourceBlock_.updateType_(option);
                 if (this.sourceBlock_[handler])
