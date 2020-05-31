fa8b379bc57255e3196cc75a5a9eff4a9d4809de
vlebedynska <v.lebedynska@gmail.com>
2020-06-01 00:03:47 +0200

    #7 defined blocks for the reinforcement learning extension

--- blocks/ai.js
+++ blocks/ai.js
@@ -66,7 +66,7 @@ Blockly.Blocks['ai_nn_input_node_coloursensor_rgb_channel'] = {
         this.setColour(Blockly.CAT_AI_RGB);
         var colorField = new Blockly.FieldColour('#DC143C');
         colorField
-            .setColours([ '#DC143C', '#0057A6', '#00FF00'])
+            .setColours([ Blockly.Msg.RED, '#0057A6', '#00FF00'])
             .setColumns(3);
 
         var port = new Blockly.FieldDropdown([
@@ -95,12 +95,6 @@ Blockly.Blocks['ai_nn_output_node'] = {
      */
     
     init: function () {
-/*      var ports = [];
-        switch (this.workspace.device) {
-                    case 'ev3':
-                        ports = [ [ Blockly.Msg.MOTOR_PORT + ' A', 'A' ], [ Blockly.Msg.MOTOR_PORT + ' B', 'B' ], [ Blockly.Msg.MOTOR_PORT + ' C', 'C' ],
-                            [ Blockly.Msg.MOTOR_PORT + ' D', 'D' ] ];
-                        break;*/
         //TODO it's now done only for EV3 - add other robot systems in the future
         var ports = [];
         ports = [
@@ -115,7 +109,6 @@ Blockly.Blocks['ai_nn_output_node'] = {
             .appendField(aiOutput, 'OUTPUTNODE')
         this.setTooltip(Blockly.Msg.VARIABLE_TYPE_NN_OUTPUT_TOOLTIP);
     }
-
 }
 
 
@@ -126,5 +119,178 @@ Blockly.Blocks['ai_hidden'] = {
       */
     init: function () {
 
+    }
+}
+
+
+
+
+Blockly.Blocks['ai_q_learning_states_and_actions_matrix'] = {
+    init : function () {
+        this.setInputsInline(false);
+
+        this.setColour(Blockly.CAT_AI_RGB);
+        this.appendDummyInput().appendField( 'Gestalte die Karte 3x3')
+
+        var start = new Blockly.FieldDropdown([
+            ["A", 'A'],
+            ["B", 'B'],
+            ["C", 'C'],
+            ["D", 'D'],
+            ["E", 'E'],
+            ["F", 'F'],
+            ["G", 'G'],
+            ["H", 'H'],
+            ["I", 'I'],
+
+        ]);
+
+        var goal = new Blockly.FieldDropdown([
+            ["I", 'I'],
+            ["A", 'A'],
+            ["B", 'B'],
+            ["C", 'C'],
+            ["D", 'D'],
+            ["E", 'E'],
+            ["F", 'F'],
+            ["G", 'G'],
+            ["H", 'H'],
+        ]);
+
+
+        this.appendDummyInput()
+            .appendField("Start")
+            .appendField(start, 'Start')
+            .appendField("Ziel")
+            .appendField( goal,'Ziel');
+
+        this.appendValueInput("OUTPUT_LAYER").appendField('Setze Hindernisse').setCheck("Array_OutputNode");
+        // this.appendDummyInput()
+        //     .appendField(new Blockly.FieldImage("./map(2).svg ", 100, 100, { alt: "*", flipRtl: "FALSE" }));
+
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setTooltip(Blockly.Msg.NEURAL_NETWORK_TOOLTIP);
+    }
+}
+
+Blockly.Blocks['ai_q_barrier_mountain'] = {
+    init : function () {
+
+        var start = new Blockly.FieldDropdown([
+            ["A", 'A'],
+            ["B", 'B'],
+            ["C", 'C'],
+            ["D", 'D'],
+            ["E", 'E'],
+            ["F", 'F'],
+            ["G", 'G'],
+            ["H", 'H'],
+            ["I", 'I'],
+
+        ]);
+
+        var goal = new Blockly.FieldDropdown([
+            ["I", 'I'],
+            ["A", 'A'],
+            ["B", 'B'],
+            ["C", 'C'],
+            ["D", 'D'],
+            ["E", 'E'],
+            ["F", 'F'],
+            ["G", 'G'],
+            ["H", 'H'],
+        ]);
+
+
+        this.appendDummyInput()
+            .appendField("Berg zwischen ")
+            .appendField(start, 'Start')
+            .appendField("und")
+            .appendField( goal,'Ziel');
+
+        this.setOutput(true, 'OutputNode');
+
+
+    }
+}
+
+
+Blockly.Blocks['ai_q_drive_the_best_way'] = {
+    init : function () {
+
+        this.appendValueInput("OUTPUT_LAYER").appendField('Fahre den optimalen Weg mit der Geschwindigkeit ').setCheck('Number');
+
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setTooltip(Blockly.Msg.NEURAL_NETWORK_TOOLTIP);
+    }
+    }
+
+Blockly.Blocks['ai_q_apply_learning_rule'] = {
+    init : function () {
+
+        this.appendDummyInput().appendField("Führe einen Lernschritt aus");
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setTooltip(Blockly.Msg.NEURAL_NETWORK_TOOLTIP);
+    }
+}
+
+
+Blockly.Blocks['ai_q_learner_config'] = {
+    init : function () {
+        this.setInputsInline(false);
+
+        this.appendDummyInput()
+            .appendField("Set up the learning behaviour")
+
+        var alpha = new Blockly.FieldDropdown([
+            ["langsam", '0.1'],
+            ["mittel", '0.5'],
+            ["schnell", '0.9'],
+
+        ]);
+
+        var gamma = new Blockly.FieldDropdown([
+            ["sofort", '0.2'],
+            ["später", '0.8'],
+        ]);
+
+        var nu = new Blockly.FieldDropdown([
+            ["Bleib auf dem Pfad", 'zufällig'],
+            ["Suche den Startpunkt zufällig", 'nicht zufallig'],
+        ]);
+
+        var rho = new Blockly.FieldDropdown([
+            ["Keine: auf zu den neuen Ufern! ", '0.1'],
+            ["Teils/Teils", '0.5'],
+            ["Viel: Nutze dein bestehendes Wissen aus", '0.9'],
+        ]);
+
+
+
+
+        this.appendDummyInput()
+            .appendField("Lerntempo")
+            .appendField(alpha, 'alpha')
+        this.appendDummyInput()
+            .appendField("Belohnung")
+            .appendField(gamma, 'gamma')
+        this.appendDummyInput()
+            .appendField("Startposition")
+            .appendField(nu, 'nu')
+        this.appendDummyInput()
+            .appendField("Erfahrung")
+            .appendField(rho, 'rho')
+
+
+
+        //this.appendValueInput("OUTPUT_LAYER").appendField('Fahre den optimalen Weg mit der Geschwindigkeit ').setCheck('Number');
+
+
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setTooltip(Blockly.Msg.NEURAL_NETWORK_TOOLTIP);
     }
 }
